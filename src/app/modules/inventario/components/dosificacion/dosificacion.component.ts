59161ba3eba4eb2e3dd4c3fead5da30018c011b8
jmfernanda <50030932+jmfernanda@users.noreply.github.com>
2021-12-23 08:49:17 -0500

    Crear Dosificacion
    Se suben cambios para guardar una dosificación utilizando ventanas modales.

--- src/app/modules/inventario/components/dosificacion/dosificacion.component.ts
+++ src/app/modules/inventario/components/dosificacion/dosificacion.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { Dosificacion } from 'src/app/shared/models/dosificacion';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { CrearDosificacionComponent } from './crear-dosificacion/crear-dosificacion.component';
+import { DosificacionService } from '../../services/dosificacion.service';
 
 @Component({
   selector: 'app-dosificacion',
@@ -8,20 +11,42 @@ import { Dosificacion } from 'src/app/shared/models/dosificacion';
   styleUrls: ['./dosificacion.component.css'],
 })
 export class DosificacionComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'nombre', 'descripcion', 'codigo'];
-
-  dosificacionData: Dosificacion[] = [
-    { id: 1, nombre: 'Hydrogen', descripcion: 'Descripcion', codigo: 'H' },
-    { id: 2, nombre: 'Hydro', descripcion: 'Descripcion', codigo: 'H' },
-    { id: 3, nombre: 'Hogen', descripcion: 'Descripcion', codigo: 'H' },
-  ];
+  dosificacionesData: Dosificacion[] = [];
+  displayedColumns: string[] = ['id', 'nombre', 'codigo'];
 
   dataSource!: MatTableDataSource<Dosificacion>;
 
-  constructor() {}
+  constructor(
+    public dialog: MatDialog,
+    public dosificacionService: DosificacionService
+  ) {}
 
   ngOnInit(): void {
+    this.loadTableDosificacion();
+  }
+
+  openCreateDialog() {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.width = '50%';
+    dialogConfig.panelClass = 'dialog-custom';
+    const dialogRef = this.dialog.open(
+      CrearDosificacionComponent,
+      dialogConfig
+    );
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!!result) {
+        this.loadDosificacion();
+        this.loadTableDosificacion();
+      }
+    });
+  }
+
+  loadDosificacion() {
+    return this.dosificacionService.getDosificaciones();
+  }
+
+  loadTableDosificacion() {
     this.dataSource = new MatTableDataSource<Dosificacion>([]);
-    this.dataSource.data = this.dosificacionData;
+    this.dataSource.data = this.loadDosificacion();
   }
 }
