4e59d9f4003c3589eb12a2622032d12ced5b490a
jmfernanda <50030932+jmfernanda@users.noreply.github.com>
2021-12-27 17:05:52 -0500

    FinalizacionCrud
    - Se finaliza el crud de medicamentos

--- src/app/modules/inventario/components/medicamento/editar-medicamento/editar-medicamento.component.ts
+++ src/app/modules/inventario/components/medicamento/editar-medicamento/editar-medicamento.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Dosificacion } from 'src/app/shared/models/dosificacion';
 import { Laboratorio } from 'src/app/shared/models/laboratorio';
 import { Medicamento } from 'src/app/shared/models/medicamento';
 import Swal from 'sweetalert2';
+import { DosificacionService } from '../../../services/dosificacion.service';
+import { LaboratorioService } from '../../../services/laboratorio.service';
 import { MedicamentoService } from '../../../services/medicamento.service';
-import { CrearMedicamentoComponent } from '../crear-medicamento/crear-medicamento.component';
 
 @Component({
   selector: 'app-editar-medicamento',
@@ -22,42 +23,59 @@ export class EditarMedicamentoComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private medicamentoService: MedicamentoService,
-    public dialogRef: MatDialogRef<CrearMedicamentoComponent>
+    private laboratorioService: LaboratorioService,
+    private dosificacionService: DosificacionService,
+    public dialogRef: MatDialogRef<EditarMedicamentoComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Medicamento
   ) {
     this.form = this.fb.group({
-      nombreCtrl: ['', [Validators.required, Validators.maxLength(20)]],
-      precioCtrl: ['', [Validators.required, Validators.maxLength(10)]],
-      cantidadCtrl: ['', [Validators.required, Validators.maxLength(4)]],
-      codBarrasCtrl: ['', [Validators.required, Validators.maxLength(15)]],
-      codCompraCtrl: ['', [Validators.required, Validators.maxLength(15)]],
+      nombreCtrl: [data.nombre, [Validators.required, Validators.maxLength(20)]],
+      precioCtrl: [data.precio, [Validators.required, Validators.maxLength(10)]],
+      cantidadCtrl: [data.cantidad, [Validators.required, Validators.maxLength(4)]],
+      codBarrasCtrl: [data.codBarras, [Validators.required, Validators.maxLength(15)]],
+      codCompraCtrl: [data.codCompra, [Validators.required, Validators.maxLength(15)]],
+      dosificacionCtrl: [data.nombreDosificacion, [Validators.required]],
+      laboratorioCtrl: [data.nombreLaboratorio, [Validators.required]],
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.loadDosificaciones();
+    this.loadLaboratorios();
+  }
 
-  loadLaboratorios() {}
+  loadLaboratorios() {
+    this.dataLaboratorios = this.laboratorioService.getLaboratorios();
+  }
 
-  loadDosificaciones() {}
+  loadDosificaciones() {
+    this.dataDosificaciones = this.dosificacionService.getDosificaciones();
+  }
 
   editarMedicamento() {
     if (this.form.valid) {
       let medicamento = new Medicamento();
-      /*laboratorio.id = this.data.id;
-      laboratorio.codigo = this.form.value['codigoCtrl'];
-      laboratorio.nombre = this.form.value['nombreCtrl'];
+      medicamento.id = this.data.id;
+      medicamento.nombre = this.form.value['nombreCtrl'];
+      medicamento.precio = this.form.value['precioCtrl'];
+      medicamento.cantidad = this.form.value['cantidadCtrl'];
+      medicamento.codBarras = this.form.value['codBarrasCtrl'];
+      medicamento.codCompra = this.form.value['codCompraCtrl'];
+      medicamento.nombreDosificacion = this.form.value['dosificacionCtrl'];
+      medicamento.nombreLaboratorio = this.form.value['laboratorioCtrl'];
 
       Object.assign(
-        laboratorio,
-        this.laboratorioService.editarLaboratorio(laboratorio)
+        medicamento,
+        this.medicamentoService.editarMedicamento(medicamento)
       );
-      this.dialogRef.close(laboratorio);*/
+      this.dialogRef.close(medicamento);
     }
   }
 
   confirmModal() {
     Swal.fire({
       title: 'Correcto',
-      text: 'Medicamento actualizado exitosamente!',
+      text: '¡Medicamento actualizado exitosamente!',
       icon: 'success',
       showConfirmButton: false,
       timer: 2000,
